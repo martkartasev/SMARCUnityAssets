2ede5ee0b45b6a0f7a9291bcbdb5fcf8c5cc1a74
Mart Kartasev <mart.kartasev@gmail.com>
2023-11-17 14:49:35 +0200

    Articulation testing

--- Runtime/Scripts/Force/ForcePoint.cs
+++ Runtime/Scripts/Force/ForcePoint.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using DefaultNamespace;
 using DefaultNamespace.Water;
 using UnityEngine;
@@ -10,6 +11,7 @@ using UnityEngine;
 public class ForcePoint : MonoBehaviour
 {
     private Rigidbody _rigidbody;
+    private ArticulationBody _articulationBody;
     private int _pointCount;
 
     private WaterQueryModel _waterModel;
@@ -23,35 +25,68 @@ public class ForcePoint : MonoBehaviour
 
     public void Awake()
     {
-        if(motionModel == null) Debug.Log("ForcePoints require a motionModel object with a rigidbody to function!");
+        if (motionModel == null) Debug.Log("ForcePoints require a motionModel object with a rigidbody or child articulation bodies to function!");
         _rigidbody = motionModel.GetComponent<Rigidbody>();
+        if (_rigidbody == null)
+        {
+            _articulationBody = motionModel
+                .GetComponentsInChildren<ArticulationBody>()
+                .First(body => body.isRoot);
+            _articulationBody.useGravity = false;
+        }
+        else
+        {
+            _rigidbody.useGravity = false;
+        }
+
+        if (motionModel == null && _articulationBody == null)
+        {
+            Debug.Log("No child articulation bodies or rigidbody found!");
+        }
+
         _waterModel = FindObjectsByType<WaterQueryModel>(FindObjectsSortMode.None)[0];
         _forceModel = motionModel.GetComponent<IForceModel>();
-        _rigidbody.useGravity = false;
+
         _pointCount = transform.parent.gameObject.GetComponentsInChildren<ForcePoint>().Length;
     }
 
     private void FixedUpdate()
     {
         var forcePointPosition = transform.position;
+        if (_rigidbody != null)
+        {
+            _rigidbody.AddForceAtPosition(Physics.gravity / _pointCount, forcePointPosition);
+        }
+        else
+        {
+            _articulationBody.AddForceAtPosition(Physics.gravity / _pointCount, forcePointPosition);
+        }
 
-        _rigidbody.AddForceAtPosition(Physics.gravity / _pointCount, forcePointPosition, ForceMode.Acceleration);
 
         float waterSurfaceLevel = _waterModel.GetWaterLevelAt(forcePointPosition);
         if (forcePointPosition.y < waterSurfaceLevel)
         {
             float displacementMultiplier = Mathf.Clamp01((waterSurfaceLevel - forcePointPosition.y) / depthBeforeSubmerged) * displacementAmount;
-            _rigidbody.AddForceAtPosition(
-                new Vector3(0, Math.Abs(Physics.gravity.y) * displacementMultiplier / _pointCount, 0),
-                forcePointPosition,
-                ForceMode.Acceleration);
+
+            if (_rigidbody != null)
+            {
+                _rigidbody.AddForceAtPosition(
+                    new Vector3(0, Math.Abs(Physics.gravity.y) * displacementMultiplier / _pointCount, 0),
+                    forcePointPosition);
+            }
+            else
+            {
+                _articulationBody.AddForceAtPosition(
+                    new Vector3(0, Math.Abs(Physics.gravity.y) * displacementMultiplier / _pointCount, 0),
+                    forcePointPosition);
+            }
+
 
             if (_forceModel != null)
             {
-            //    _rigidbody.AddRelativeForce(_forceModel.GetForceDamping() / _pointCount, ForceMode.Force);
-            //    _rigidbody.AddRelativeTorque(_forceModel.GetTorqueDamping() / _pointCount, ForceMode.Force);
+                //    _rigidbody.AddRelativeForce(_forceModel.GetForceDamping() / _pointCount, ForceMode.Force);
+                //    _rigidbody.AddRelativeTorque(_forceModel.GetTorqueDamping() / _pointCount, ForceMode.Force);
             }
         }
-
     }
 }
---
+++ Runtime/Scripts/Force/SAMArticulationModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using UnityEngine;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using Unity.Mathematics;
+using Unity.Robotics.UrdfImporter;
+
+namespace DefaultNamespace
+{
+    public class SAMArticulationModel : MonoBehaviour, IForceModel, ISAMControl
+    {
+        public ArticulationBody baseLink;
+        public ArticulationBody nozzleLink;
+        public ArticulationBody thrustLink;
+        public ArticulationBody propeller1;
+        public ArticulationBody propeller2;
+        public double lcg { get; set; }
+        public double vbs { get; set; }
+        public float d_rudder { get; set; }
+        public float d_aileron { get; set; }
+        public double rpm1 { get; set; }
+        public double rpm2 { get; set; }
+
+        public void Start()
+        {
+            baseLink.enabled = true;
+            nozzleLink.enabled = true;
+            thrustLink.enabled = true;
+            propeller1.enabled = true;
+            propeller2.enabled = true;
+        }
+
+        public void SetRpm(double rpm1, double rpm2)
+        {
+            this.rpm1 = rpm1;
+            this.rpm2 = rpm2;
+        }
+
+        public void SetRudderAngle(float dr)
+        {
+            d_rudder = dr;
+        }
+
+        public void SetElevatorAngle(float de)
+        {
+            d_aileron = de;
+        }
+
+        public void SetBatteryPack(double lcg)
+        {
+            this.lcg = lcg;
+        }
+
+        public void SetWaterPump(double vbs)
+        {
+            this.vbs = vbs;
+        }
+
+        private void FixedUpdate()
+        {
+            var r1 = rpm1 / 2000; // Normalize with assumption that max == 2000
+            var r2 = rpm2 / 2000;
+
+
+            nozzleLink.SetDriveTarget(ArticulationDriveAxis.Y, -Mathf.Rad2Deg * d_rudder);
+            nozzleLink.SetDriveTarget(ArticulationDriveAxis.Z, Mathf.Rad2Deg * d_aileron);
+
+            thrustLink.SetDriveTarget(ArticulationDriveAxis.X, (float)(r1 * thrustLink.xDrive.upperLimit));
+
+            propeller1.SetDriveTargetVelocity(ArticulationDriveAxis.X, (float)(r1 * 2000));
+            propeller2.SetDriveTargetVelocity(ArticulationDriveAxis.X, (float)(-r2 * 2000));
+
+            thrustLink.AddRelativeForce(new Vector3(0, 0, 10) * (float)r1);
+        }
+
+
+        public Vector3 GetTorqueDamping()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Vector3 GetForceDamping()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
