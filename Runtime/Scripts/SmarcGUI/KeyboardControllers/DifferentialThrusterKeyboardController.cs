57ba37ffdd67efd862cbb4483036b821e3d93926
Ozer Ozkahraman <ozkahramanozer@gmail.com>
2025-11-05 12:03:58 +0100

    Added floatsam_usv. No sensors or controls until needed. Keyboardable

---
+++ Runtime/Scripts/SmarcGUI/KeyboardControllers/DifferentialThrusterKeyboardController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+using Propeller = VehicleComponents.Actuators.Propeller;
+
+namespace SmarcGUI.KeyboardControllers
+{
+    public class DifferentialThrusterKeyboardController : KeyboardControllerBase
+    {
+
+        public Propeller propLeft, propRight;
+        public float moveRpms = 1500f;
+
+
+        InputAction forwardAction, tvAction;
+        
+
+        void Awake()
+        {
+            forwardAction = InputSystem.actions.FindAction("Robot/Forward");
+            tvAction = InputSystem.actions.FindAction("Robot/ThrustVector");
+        }
+
+        void Update()
+        {
+            var forwardValue = forwardAction.ReadValue<float>();
+            var tv = tvAction.ReadValue<Vector2>();
+            var rudderValue = tv.x;
+
+            propLeft.SetRpm((forwardValue + rudderValue) * moveRpms);
+            propRight.SetRpm((forwardValue - rudderValue) * moveRpms);
+        }
+
+        public override void OnReset()
+        {
+            propLeft.SetRpm(0f);
+            propRight.SetRpm(0f);
+        }
+
+    }
+}
