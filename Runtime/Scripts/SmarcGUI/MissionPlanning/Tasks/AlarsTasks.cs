adcfc2aef81f3f79586f3f7171807757e5af3e3a
Ozer Ozkahraman <ozkahramanozer@gmail.com>
2025-11-17 22:42:18 +0100

    M350 camera matches real, again. Alars tasks

--- Runtime/Scripts/SmarcGUI/MissionPlanning/Tasks/AlarsTasks.cs
+++ Runtime/Scripts/SmarcGUI/MissionPlanning/Tasks/AlarsTasks.cs
@@ -12,13 +12,10 @@ namespace SmarcGUI.MissionPlanning.Tasks
             Params.Add("initial_travel_alt", 25.0f);
             Params.Add("search_position", new GeoPoint());
             Params.Add("delivery_position", new GeoPoint());
-            Params.Add("recover_min_height_above_water", 3.0f);
-            Params.Add("recover_swoop_vertical", 5.0f);
-            Params.Add("recover_swoop_horizontal", 1.0f);
-            Params.Add("recover_straight_before_rope", 1.0f);
-            Params.Add("recover_straight_distance", 2.0f);
-            Params.Add("recover_raise_horizontal", 1.0f);
-            Params.Add("recover_raise_vertical", 15.0f);
+            Params.Add("forward_distance", 2.0f);
+            Params.Add("forward_altitude", 3.0f);
+            Params.Add("dipping_altitude", 7.0f);
+            Params.Add("raising_altitude", 15.0f);
         }
     }
 
@@ -40,13 +37,10 @@ namespace SmarcGUI.MissionPlanning.Tasks
             Description = "Hook a rope in the water";
             Params.Add("object_position", new GeoPoint());
             Params.Add("buoy_position", new GeoPoint());
-            Params.Add("min_height_above_water", 3.0f);
-            Params.Add("swoop_vertical", 5.0f);
-            Params.Add("swoop_horizontal", 3.0f);
-            Params.Add("straight_before_rope", 1.0f);
-            Params.Add("straight_distance", 3.0f);
-            Params.Add("raise_horizontal", 1.0f);
-            Params.Add("raise_vertical", 10.0f);
+            Params.Add("forward_distance", 2.0f);
+            Params.Add("forward_altitude", 3.0f);
+            Params.Add("dipping_altitude", 7.0f);
+            Params.Add("raising_altitude", 15.0f);
         }
     }
 
@@ -57,23 +51,8 @@ namespace SmarcGUI.MissionPlanning.Tasks
             Name = "alars-localize";
             Description = "Localize an AUV in the water by moving above the camera detection points";
             Params.Add("localize_auv", true);
-            Params.Add("localize_buoy", true);
+            Params.Add("localize_buoy", false);
         }
     }
-
-    public class AlarsCheckLoad : Task
-    {
-        public override void SetParams()
-        {
-            Name = "alars-check-load";
-            Description = "Check if the AUV is hooked by measuring the load on the hook";
-            Params.Add("check_duration", 2.0f);
-            Params.Add("override_result", -1);
-        }
-    }
-    
-    
-
-    
 
 }
