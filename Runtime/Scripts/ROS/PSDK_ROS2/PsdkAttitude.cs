db3b4edd0a774b6e2f8aa6f8e4ec2b861cfefd74
Ozer Ozkahraman <ozkahramanozer@gmail.com>
2026-02-03 12:11:36 +0100

    fix psdk heading hack back to not-a-hack

--- Runtime/Scripts/ROS/PSDK_ROS2/PsdkAttitude.cs
+++ Runtime/Scripts/ROS/PSDK_ROS2/PsdkAttitude.cs
@@ -22,8 +22,7 @@ namespace M350.PSDK_ROS2
         protected override void UpdateMessage()
         {
             var quaternion = body.transform.rotation;
-            var rot_quat = Quaternion<ENU>.AngleAxis(-90f, Vector3<ENU>.up);
-            ROSMsg.quaternion = quaternion.To<ENU>() * rot_quat; //Rotate by 90 degrees to align heading with front of drone
+            ROSMsg.quaternion = quaternion.To<ENU>();
             ROSMsg.header.frame_id = "odom";
             ROSMsg.header.stamp = new TimeStamp(Clock.time);
         }
