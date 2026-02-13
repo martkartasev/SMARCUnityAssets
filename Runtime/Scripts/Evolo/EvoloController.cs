66e5bbcc11c531e91606ff6c05445b1117aa148f
Ozer Ozkahraman <ozkahramanozer@gmail.com>
2026-02-13 15:25:40 +0100

    Added generic twist actuator interface and subscriber to command velocities of things

--- Runtime/Scripts/Evolo/EvoloController.cs
+++ Runtime/Scripts/Evolo/EvoloController.cs
@@ -1,12 +1,13 @@
 using Smarc.GenericControllers;
 using UnityEngine;
+using VehicleComponents.Actuators;
 
 namespace Evolo
 {
     [RequireComponent(typeof(AltitudeController))]
     [RequireComponent(typeof(AttitudeController))]
     [RequireComponent(typeof(HorizontalController))]
-    public class EvoloController : MonoBehaviour
+    public class EvoloController : MonoBehaviour, IGenericTwistActuator
     {
         [Header("Targets")]
         public float Speed;
@@ -21,6 +22,11 @@ namespace Evolo
         AltitudeController altCtrl;
         AttitudeController attCtrl;
         HorizontalController horizCtrl;
+
+        Vector3 twistLinear = Vector3.zero;
+        Vector3 twistAngular = Vector3.zero;
+
+        
 
         void Awake()
         {
@@ -42,10 +48,33 @@ namespace Evolo
         {
             Speed = Mathf.Clamp(Speed, -MaxSpeed, MaxSpeed);
             horizCtrl.TargetVelocity = new Vector3(0, 0, Mathf.Clamp(Speed, -MaxSpeed, MaxSpeed));
+            twistLinear.z = Speed;
             YawRate = Mathf.Clamp(YawRate, -MaxYawRate, MaxYawRate);
+            twistAngular.y = YawRate;
             attCtrl.TargetYawRate = Mathf.Clamp(YawRate, -MaxYawRate, MaxYawRate);
             Altitude = Mathf.Clamp(Altitude, 0, MaxAltitude);
             altCtrl.TargetAltitude = Altitude;
+        }
+
+        public (Vector3, Vector3) GetCurrentValue()
+        {
+            return (twistLinear, twistAngular);
+        }
+
+        public (Vector3, Vector3) GetResetValue()
+        {
+            return (Vector3.zero, Vector3.zero);
+        }
+
+        public bool HasNewData()
+        {
+            return true;
+        }
+
+        public void SetTwist(Vector3 LinearVelocity, Vector3 AngularVelocity)
+        {
+            Speed = LinearVelocity.z;
+            YawRate = AngularVelocity.y;
         }
     }
 }
